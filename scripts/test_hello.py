a5cf646ce7b304d6ece7a5edf1140e67f4af211a
hitfeat <haoyuexing@itfeat.com>
2019-10-22 15:13:04 +0800

    增加了xxx功能

--- scripts/test_hello.py
+++ scripts/test_hello.py
@@ -1,10 +1,13 @@
 class TestHello:
 
     def test_hello1(self):
-        print("hello")
+        print("hello123")
 
     def test_hello2(self):
         print("3333")
         
     def test_hello3(self):
         print("hello3")
+
+    def test_hello4(self):
+        print("hello4")
---
+++ scripts/test_logout.py
@@ -0,0 +1,3 @@
+class TestLogout:
+
+    pass
